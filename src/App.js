025dbb5c6250f6ee632e40ae3c5f461138f4ef7b
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-16 17:44:13 +0100

    Redux setup and data loaded from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { React, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCovidData } from './redux/covid';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCovidData());
+  }, []);
+
   return (
     <div className="App">
       Hello
