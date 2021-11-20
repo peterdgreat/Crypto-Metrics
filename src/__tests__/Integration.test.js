fd897092024385a5cad3220f724ff407ec17f925
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-20 21:52:00 +0100

    Integration test added

--- src/__tests__/Integration.test.js
+++ src/__tests__/Integration.test.js
@@ -1,6 +1,13 @@
+import { render, screen } from '@testing-library/react';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import { fetchCryptoData } from '../redux/crypto';
+import store from '../redux/configurestore';
+import Crypto from '../components/Crypto';
+import Search from '../components/Search';
+import App from '../App';
+
 import cryptoData from '../mock/cryptoData';
 
 const middlewares = [thunk];
@@ -21,4 +28,40 @@ it('calls request and success actions if the fetch response was successful', () 
       const expectedActions = store.getActions();
       expect(expectedActions.length).toBe(1);
     });
+});
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+    const header = screen.findByText('Cryptocurrency');
+    expect(header).toBeTruthy();
+  });
+});
+
+describe('Crypto', () => {
+  test('renders Crypto component', () => {
+    render(
+      <Provider store={store}>
+        <Crypto />
+      </Provider>,
+    );
+    const header = screen.findByText('Global Stats');
+    expect(header).toBeTruthy();
+  });
+});
+
+describe('Search', () => {
+  test('renders Seatch component', () => {
+    render(
+      <Provider store={store}>
+        <Search />
+      </Provider>,
+    );
+    const search = screen.findByRole('textbox');
+    expect(search).toBeTruthy();
+  });
 });
